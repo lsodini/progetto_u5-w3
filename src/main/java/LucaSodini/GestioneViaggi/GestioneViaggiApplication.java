1e86019180f0727236ca78d56770abe5110e8bb8
lsodini <lucasodini5@gmail.com>
2024-11-08 13:42:06 +0100

    Initial commit

---
+++ src/main/java/LucaSodini/GestioneViaggi/GestioneViaggiApplication.java
@@ -0,0 +1,13 @@
+package LucaSodini.GestioneViaggi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestioneViaggiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestioneViaggiApplication.class, args);
+	}
+
+}
